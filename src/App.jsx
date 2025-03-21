ee50082a0b683be5f7462c68f677b430497cadef
bishwas350 <crashmindboy10@gmail.com>
2025-03-22 02:06:44 +0600

    Breadcrumb added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Rootlayout from './pages/layout/Rootlayout'
 import About from './pages/About'
+import Breadcrumb from './component/Breadcrumb'
 
 function App() {
 

83f6db7b98b83737a46a47cfb3e901424fc64f2a
bishwas350 <crashmindboy10@gmail.com>
2025-03-23 00:44:18 +0600

    team part added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import EmailPart from '../component/EmailPart'
+import TeamsPart from '../component/TeamsPart'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <TeamsPart/>
+    <EmailPart/>
+    </>
   )
 }
 

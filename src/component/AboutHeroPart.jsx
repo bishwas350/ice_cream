cee71af26239197478336fff5ef9c2a92feb5eff
bishwas350 <crashmindboy10@gmail.com>
2025-03-23 01:21:52 +0600

    AboutHeroPart added

---
+++ src/component/AboutHeroPart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutHeroPart = () => {
+  return (
+    <>
+    <div className="bg-AboutBg w-full bg-contain bg-center bg-no-repeat py-16 sm:py-24 md:py-32 lg:py-52 min-h-[300px] sm:min-h-[400px] md:min-h-[500px] lg:min-h-[600px] ">
+         
+    </div>
+    </>
+  )
+}
+
+export default AboutHeroPart
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import EmailPart from '../component/EmailPart'
 import TeamsPart from '../component/TeamsPart'
+import AboutHeroPart from '../component/AboutHeroPart'
 
 const About = () => {
   return (
     <>
+    <AboutHeroPart/>
     <TeamsPart/>
     <EmailPart/>
     </>

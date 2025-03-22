3f93363f28196e56e9e964c77adc079691a23727
bishwas350 <crashmindboy10@gmail.com>
2025-03-23 02:06:44 +0600

    journy part added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import EmailPart from '../component/EmailPart'
 import TeamsPart from '../component/TeamsPart'
 import AboutHeroPart from '../component/AboutHeroPart'
+import JourneyPart from '../component/JourneyPart'
 
 const About = () => {
   return (
     <>
+    <JourneyPart/>
     <AboutHeroPart/>
     <TeamsPart/>
     <EmailPart/>

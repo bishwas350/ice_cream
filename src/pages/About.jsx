4174cd6624610ba75a0f4469c545c08c38b4c947
bishwas350 <crashmindboy10@gmail.com>
2025-03-23 03:56:12 +0600

    counterPart added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,14 @@ import EmailPart from '../component/EmailPart'
 import TeamsPart from '../component/TeamsPart'
 import AboutHeroPart from '../component/AboutHeroPart'
 import JourneyPart from '../component/JourneyPart'
+import CounterPart from '../component/CounterPart'
 
 const About = () => {
   return (
     <>
     <JourneyPart/>
     <AboutHeroPart/>
+    <CounterPart/>
     <TeamsPart/>
     <EmailPart/>
     </>
